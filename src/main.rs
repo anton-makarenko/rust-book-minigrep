1114b7eadaf11db8874b675f607a63eda13f192f
Anton Makarenko <makarenkoanton2013@gmail.com>
2022-08-25 23:41:29 +0300

    eprintln!

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use rust_book_minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|_| {
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(_) = rust_book_minigrep::run(config) {
+    if let Err(e) = rust_book_minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }

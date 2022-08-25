5beb148529778d1a6af57bc579ad7053d6fa8d4c
Anton Makarenko <makarenkoanton2013@gmail.com>
2022-08-25 21:08:54 +0300

    Search functionality

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,11 @@ use rust_book_minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+    let config = Config::new(&args).unwrap_or_else(|_| {
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
-    if let Err(e) = rust_book_minigrep::run(config) {
-        println!("Application error: {}", e);
+    if let Err(_) = rust_book_minigrep::run(config) {
         process::exit(1);
     }
 }
